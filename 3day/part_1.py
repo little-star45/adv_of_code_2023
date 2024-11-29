9c2d6425b067165a697d6ab90e4517d7b16342ae
PatrycjaD <domanska.patrycja7@gmail.com>
2024-11-30 00:15:31 +0100

    day 3 initial

---
+++ 3day/part_1.py
@@ -0,0 +1,31 @@
+#important information:
+#red, green, blue - cubes colours
+"""
+
+
+Main task:
+
+
+"""
+
+def load_data_from_file(data_file):
+    return open(data_file,'r').read()
+
+def decode_data(data):
+    pass
+
+def main(data_file, colors_tuple):
+    pass
+
+
+def test_function(data_file):
+    answers = (8,)
+    _,__ = main(data_file)
+
+    if (_==answers[0]):
+        print('Pass') 
+    else:
+        print(f'') 
+
+if __name__ == '__main__':
+    # test_function('data1.txt')
